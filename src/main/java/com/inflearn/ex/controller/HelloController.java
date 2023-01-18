8ad1c8b88abda525839cf6eab4346bd8db607893
ThinkingMoomin <qnddj0213@gmail.com>
2023-01-18 19:36:56 +0900

    static, template, api method learn

--- src/main/java/com/inflearn/ex/controller/HelloController.java
+++ src/main/java/com/inflearn/ex/controller/HelloController.java
@@ -3,7 +3,9 @@ package com.inflearn.ex.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -22,5 +24,36 @@ public class HelloController {
         model.addAttribute("name", name);
 
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody   // HTTP body 부에 직접 데이터를 넣어주겠다.
+    public String helloString(@RequestParam String name) {
+
+        return "Hello" + name;  // HTML 태그없이 순수 스트링 데이터만 출력 된다.
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        return hello;
+    }
+
+    public static class Hello {
+
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
     }
 }
