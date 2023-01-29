2618f5dacaf3d2175d76e63e9ea72a6797e7c16f
ThinkingMoomin <qnddj0213@gmail.com>
2023-01-29 21:53:59 +0900

    Inquire member List learn

--- src/main/java/com/inflearn/ex/controller/MemberController.java
+++ src/main/java/com/inflearn/ex/controller/MemberController.java
@@ -4,8 +4,11 @@ import com.inflearn.ex.domain.Member;
 import com.inflearn.ex.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -35,5 +38,14 @@ public class MemberController {
         System.out.println("member = "+ member.getName());
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
