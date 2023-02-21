4fc3bfa93a728a1281a9a8ee6acdc7c274704519
ThinkingMoomin <qnddj0213@gmail.com>
2023-02-22 00:05:08 +0900

    JPA library learn

--- src/main/java/com/inflearn/ex/SpringConfig.java
+++ src/main/java/com/inflearn/ex/SpringConfig.java
@@ -2,21 +2,35 @@ package com.inflearn.ex;
 
 import com.inflearn.ex.repository.JdbcMemberRepository;
 import com.inflearn.ex.repository.JdbcTemplateMemberRepository;
+import com.inflearn.ex.repository.JpaMemberRepository;
 import com.inflearn.ex.repository.MemberRepository;
 import com.inflearn.ex.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    /***
+     * JDBC, JDBC Template
+
+     private DataSource dataSource;
 
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
+    } */
+
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
+
 
     @Bean
     public MemberService memberService() {
@@ -27,6 +41,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
         // return new MemoryMemberRepository();
         // return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        // return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
