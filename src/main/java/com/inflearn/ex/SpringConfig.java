a218315c4589e07c5111da4ab9541d25ef70f438
ThinkingMoomin <qnddj0213@gmail.com>
2023-02-22 22:52:03 +0900

    JPA Data learn

--- src/main/java/com/inflearn/ex/SpringConfig.java
+++ src/main/java/com/inflearn/ex/SpringConfig.java
@@ -1,16 +1,10 @@
 package com.inflearn.ex;
 
-import com.inflearn.ex.repository.JdbcMemberRepository;
-import com.inflearn.ex.repository.JdbcTemplateMemberRepository;
-import com.inflearn.ex.repository.JpaMemberRepository;
 import com.inflearn.ex.repository.MemberRepository;
 import com.inflearn.ex.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
@@ -24,24 +18,32 @@ public class SpringConfig {
         this.dataSource = dataSource;
     } */
 
+    /***
+     * JPA
     private EntityManager em;
 
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
-    }
+    } */
 
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
+    /* @Bean
     public MemberRepository memberRepository() {
         // return new MemoryMemberRepository();
         // return new JdbcMemberRepository(dataSource);
         // return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+        // return new JpaMemberRepository(em);
+    } */
 }
---
+++ src/main/java/com/inflearn/ex/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,13 @@
+package com.inflearn.ex.repository;
+
+import com.inflearn.ex.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    // JPQL select m from Member m where m.name = ?
+    @Override
+    Optional<Member> findByName(String name);
+}
